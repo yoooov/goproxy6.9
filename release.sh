e51a23c5398528a210bbfe6a2c904c8d77c9fc4a
yoooov <yoooov@protonmail.com>
2021-01-13 20:56:04 +0100

    Update release.sh

--- release.sh
+++ release.sh
@@ -3,8 +3,8 @@ VERSION=$(cat VERSION)
 VER="${VERSION}_$(date '+%Y%m%d%H%M%S')"
 X="-X main.APP_VERSION=$VER"
 RELEASE="release-${VERSION}"
-TRIMPATH1="/Users/snail/go/src/github.com/snail007"
-TRIMPATH=$(dirname ~/go/src/github.com/snail007)/snail007
+TRIMPATH1="${GOPATH}/src/github.com/yoooov"
+TRIMPATH=$(dirname ~/go-workspace/src/github.com/yoooov)/yoooov
 if [ -d "$TRIMPATH1" ];then
 	TRIMPATH=$TRIMPATH1
 fi
