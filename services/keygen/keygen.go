bec7bdf6fc7f0e7aee5e2da28341bd5caae4ef8f
yoooov <yoooov@protonmail.com>
2021-01-13 20:15:54 +0100

    Update keygen.go

--- services/keygen/keygen.go
+++ services/keygen/keygen.go
@@ -6,9 +6,9 @@ import (
 	"os"
 	"strings"
 
-	"github.com/snail007/goproxy/services"
-	"github.com/snail007/goproxy/utils"
-	"github.com/snail007/goproxy/utils/cert"
+	"github.com/yoooov/goproxy6.9/services"
+	"github.com/yoooov/goproxy6.9/utils"
+	"github.com/yoooov/goproxy6.9/utils/cert"
 )
 
 type KeygenArgs struct {
