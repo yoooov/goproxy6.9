2969bb41a7b3c8b5f431883f9c94a8a5bd94acbe
yoooov <yoooov@protonmail.com>
2021-01-13 20:07:56 +0100

    Update release_ios.sh

--- sdk/android-ios/release_ios.sh
+++ sdk/android-ios/release_ios.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 VERSION=$(cat ../../VERSION)
 VER="${VERSION}_$(date '+%Y%m%d%H%M%S')"
-X="-X github.com/snail007/goproxy/sdk/android-ios.SDK_VERSION=$VER -X main.APP_VERSION=$VER"
+X="-X github.com/yoooov/goproxy6.9/sdk/android-ios.SDK_VERSION=$VER -X main.APP_VERSION=$VER"
 
 rm -rf sdk-ios-*.tar.gz
 rm -rf ios
