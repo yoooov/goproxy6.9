296d3c8a61ca3dcafea8206b99a212ed3b53e7f5
yoooov <yoooov@protonmail.com>
2021-01-13 20:20:56 +0100

    Update udp.go

--- services/socks/udp.go
+++ services/socks/udp.go
@@ -9,9 +9,9 @@ import (
 	"strings"
 	"time"
 
-	"github.com/snail007/goproxy/utils"
-	goaes "github.com/snail007/goproxy/utils/aes"
-	"github.com/snail007/goproxy/utils/socks"
+	"github.com/yoooov/goproxy6.9/utils"
+	goaes "github.com/yoooov/goproxy6.9/utils/aes"
+	"github.com/yoooov/goproxy6.9/utils/socks"
 )
 
 func (s *Socks) ParentUDPKey() (key []byte) {
