2b16b34d106cc048b92b8bae14980cbe587896ad
yoooov <yoooov@protonmail.com>
2021-01-13 19:55:07 +0100

    Update server.go

--- core/cs/server/server.go
+++ core/cs/server/server.go
@@ -11,12 +11,12 @@ import (
 	"runtime/debug"
 	"strconv"
 
-	tou "github.com/snail007/goproxy/core/dst"
-	compressconn "github.com/snail007/goproxy/core/lib/transport"
-	transportc "github.com/snail007/goproxy/core/lib/transport"
-	encryptconn "github.com/snail007/goproxy/core/lib/transport/encrypt"
+	tou "github.com/yoooov/goproxy6.9/core/dst"
+	compressconn "github.com/yoooov/goproxy6.9/core/lib/transport"
+	transportc "github.com/yoooov/goproxy6.9/core/lib/transport"
+	encryptconn "github.com/yoooov/goproxy6.9/core/lib/transport/encrypt"
 
-	"github.com/snail007/goproxy/core/lib/kcpcfg"
+	"github.com/yoooov/goproxy6.9/core/lib/kcpcfg"
 
 	kcp "github.com/xtaci/kcp-go"
 )
